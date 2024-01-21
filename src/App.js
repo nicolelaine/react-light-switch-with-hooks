25461599b588272f2a376007180c33df53e4eca6
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-01-21 12:01:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [isOn, setIsOn] = useState(true);
+
+  const toggleLight = () => {
+    setIsOn(!isOn);
+  };
+
+  let wallClass = "wall off";
+  if (isOn) {
+    wallClass = "wall on";
+  }
+
+  return (
+    <div className={wallClass}>
+      <div className="switch-plate">
+        <div className="screw" />
+        <div className="switch" onClick={toggleLight}>
+          <div className="switch-handle" />
+        </div>
+        <div className="screw" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
